b8a20e79d2979f26962b8ead4227fc03592583ff
pranjalsk <pkaranka@asu.edu>
2018-02-25 22:28:13 -0700

    local to  git

---
+++ Java-DS/src/com/stacksq/StackUsingQ.java
@@ -0,0 +1,54 @@
+package com.stacksq;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsingQ {
+
+	// https://leetcode.com/problems/implement-stack-using-queues/solution/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class MyStack {
+
+	Queue<Integer> q;
+
+	/** Initialize your data structure here. */
+	public MyStack() {
+		q = new LinkedList<>();
+	}
+
+	/** Push element x onto stack. */
+	public void push(int x) {
+		q.add(x);
+		for (int i = 1; i < q.size(); i++) {
+			int temp = q.poll();
+			q.add(temp);
+		}
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	public int pop() {
+		return q.poll();
+	}
+
+	/** Get the top element. */
+	public int top() {
+		return q.peek();
+	}
+
+	/** Returns whether the stack is empty. */
+	public boolean empty() {
+		return q.isEmpty();
+	}
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such: MyStack obj =
+ * new MyStack(); obj.push(x); int param_2 = obj.pop(); int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
