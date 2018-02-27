69c47bc2781cbeb7c879f4af237626b7b31d1f3e
pranjalsk <pkaranka@asu.edu>
2018-02-26 21:09:04 -0700

    local to  git

--- Java-DS/src/com/arrays/ArraySorts.java
+++ Java-DS/src/com/arrays/ArraySorts.java
@@ -33,15 +33,78 @@ public class ArraySorts {
 		}
 	}
 
+	public int binarySearch(int arr[], int x) {
+		int l = 0, r = arr.length - 1;
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			if (arr[m] == x)
+				return m;
+
+			if (arr[m] < x)
+				l = m + 1;
+			else
+				r = m - 1;
+		}
+		return -1;
+	}
+
 	public static void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
-	
-	
-	
-	
-	
-	
+	// -------------------
+
+	// Merge sort
+	public void mergesort(int arr[], int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+
+			mergesort(arr, l, m);
+			mergesort(arr, m + 1, r);
+
+			merge(arr, l, m, r);
+		}
+	}
+
+	// ---
+	void merge(int arr[], int l, int m, int r) {
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		int L[] = new int[n1];
+		int R[] = new int[n2];
+
+		for (int i = 0; i < n1; ++i)
+			L[i] = arr[l + i];
+		for (int j = 0; j < n2; ++j)
+			R[j] = arr[m + 1 + j];
+
+		int i = 0, j = 0;
+
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
 }
