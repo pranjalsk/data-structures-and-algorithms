9e6b5b9a34924f2183b4807a93ba6c3ee36af112
pranjalsk <pkaranka@asu.edu>
2018-03-01 23:12:13 -0700

    local to git

---
+++ Java-DS/src/com/arrays/Search2DMatrix.java
@@ -0,0 +1,90 @@
+package com.arrays;
+
+public class Search2DMatrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Write an efficient algorithm that searches for a value in an m x n
+	 * matrix. This matrix has the following properties:
+	 * 
+	 * Integers in each row are sorted in ascending from left to right. Integers
+	 * in each column are sorted in ascending from top to bottom.
+	 * 
+	 * For example,
+	 * 
+	 * Consider the following matrix:
+	 * 
+	 * [ [1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14,
+	 * 17, 24], [18, 21, 23, 26, 30] ]
+	 * 
+	 * Given target = 5, return true.
+	 * 
+	 * Given target = 20, return false.
+	 */
+
+	public boolean searchMatrix(int[][] matrix, int target) {
+
+		if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+			return false;
+		}
+
+		int col = matrix[0].length - 1;
+		int row = 0;
+
+		while (row < matrix.length && col >= 0) {
+
+			if (target == matrix[row][col])
+				return true;
+
+			else if (target > matrix[row][col])
+				row++;
+			else if (target < matrix[row][col])
+				col--;
+		}
+		return false;
+	}
+
+	/**
+	 * Write an efficient algorithm that searches for a value in an m x n
+	 * matrix. This matrix has the following properties:
+	 * 
+	 * Integers in each row are sorted from left to right. The first integer of
+	 * each row is greater than the last integer of the previous row.
+	 * 
+	 * For example,
+	 * 
+	 * Consider the following matrix:
+	 * 
+	 * [ [1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50] ]
+	 * 
+	 * Given target = 3, return true.
+	 * 
+	 */
+
+	public boolean searchMatrix2(int[][] matrix, int target) {
+
+		int m = matrix.length;
+		int n = matrix[0].length;
+
+		int start = 0, end = m * n - 1;
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			int mid_value = matrix[mid / n][mid % n];
+
+			if (mid_value == target)
+				return true;
+
+			else if (mid_value < target)
+				start = mid + 1;
+			else
+				end = mid - 1;
+		}
+		return false;
+	}
+
+}
--- Java-DS/src/com/dynamic/DPTrivial.java
+++ Java-DS/src/com/dynamic/DPTrivial.java
@@ -7,4 +7,80 @@ public class DPTrivial {
 
 	}
 
+	private static void towerOfHanoi(int n, String source, String auxiliary, String destination) {
+		if (n == 0)
+			return;
+		towerOfHanoi(n - 1, source, destination, auxiliary);
+		System.out.println("Disc " + n + " moved from source " + source + " to destination " + destination);
+		towerOfHanoi(n - 1, auxiliary, source, destination);
+	}
+
+	private static int fib(int n) {
+		if (n == 0 || n == 1)
+			return 1;
+		return fib(n - 1) + fib(n - 2);
+
+	}
+
+	private static void fibDP(int n) {
+		int[] F = new int[n + 1];
+		F[0] = 0;
+		F[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			F[i] = F[i - 1] + F[i - 2];
+		}
+		for (int i = 1; i <= n; i++) {
+			System.out.print(F[i] + " ");
+		}
+	}
+
+	private static int factorial(int num) {
+		if (num == 0)
+			return 0;
+		else if (num == 1)
+			return 1;
+		else
+			return num * factorial(num - 1);
+	}
+
+	private static void factorialDP(int num) {
+		int[] F = new int[num + 1];
+		F[0] = 0;
+		F[1] = 1;
+		for (int i = 2; i <= num; i++) {
+			F[i] = i * F[i - 1];
+		}
+		System.out.print(F[num]);
+	}
+
+	private static int GCD(int a, int b) {
+		if (a == 0)
+			return b;
+		return GCD(b % a, a);
+	}
+
+	private static int Division(int num1, int num2) {
+		if (num1 < num2) {
+			return 0;
+		} else {
+			return 1 + Division(num1 - num2, num2);
+		}
+	}
+
+	private static int Multiplication(int a, int b) {
+		if (b > 0) {
+			return a + Multiplication(a, b - 1);
+		} else {
+			return 0;
+		}
+	}
+
+	private static int Power(int a, int b) {
+		if (b > 0) {
+			return Multiplication(a, Power(a, b - 1));
+		} else {
+			return 1;
+		}
+	}
+
 }
