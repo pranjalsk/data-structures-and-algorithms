ff19be322f2906cc24e9b48b54f19b0466e5d92a
pranjalsk <pkaranka@asu.edu>
2018-03-06 22:06:40 -0700

    local to git

--- Java-DS/src/com/intervalProblems/MergeIntervals.java
+++ Java-DS/src/com/intervalProblems/MergeIntervals.java
@@ -47,4 +47,42 @@ public class MergeIntervals {
 		result.add(prev);
 		return result;
 	}
+
+	/**
+	 * Given a set of non-overlapping intervals, insert a new interval into the
+	 * intervals (merge if necessary).
+	 * 
+	 * You may assume that the intervals were initially sorted according to
+	 * their start times.
+	 * 
+	 * Example 1: Given intervals [1,3],[6,9], insert and merge [2,5] in as
+	 * [1,5],[6,9].
+	 * 
+	 * Example 2: Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9]
+	 * in as [1,2],[3,10],[12,16].
+	 * 
+	 * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+	 */
+	public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+
+		List<Interval> result = new ArrayList<>();
+
+		for (Interval curr : intervals) {
+
+			if (newInterval.start > curr.end) {
+				result.add(curr);
+			} else if (newInterval.end < curr.start) {
+				result.add(newInterval);
+				newInterval = curr;
+			} else if (newInterval.start <= curr.end) {
+				Integer start = Math.min(newInterval.start, curr.start);
+				Integer end = Math.max(newInterval.end, curr.end);
+				newInterval = new Interval(start, end);
+			}
+		}
+
+		result.add(newInterval);
+		return result;
+	}
+
 }
