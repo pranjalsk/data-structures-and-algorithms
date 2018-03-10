8a50b7a72f06de28cf5caa78ed57a4878bfd470c
pranjalsk <pkaranka@asu.edu>
2018-03-10 14:35:23 -0700

    local to git

--- Java-DS/src/com/permutations/PalimdromePermutations.java
+++ Java-DS/src/com/permutations/PalimdromePermutations.java
@@ -1,5 +1,58 @@
 package com.permutations;
 
+import java.util.HashMap;
+
+/**
+ * Given a string, determine if a permutation of the string could form a
+ * palindrome.
+ * 
+ * For example, "code" -> False, "aab" -> True, "carerac" -> True.
+ */
 public class PalimdromePermutations {
 
+	/**
+	 * Logic: If a string with an even length is a palindrome, every character
+	 * in the string must always occur an even number of times. If the string
+	 * with an odd length is a palindrome, every character except one of the
+	 * characters must always occur an even number of times. Thus, in case of a
+	 * palindrome, the number of characters with odd number of occurences can't
+	 * exceed 1(1 in case of odd length and 0 in case of even length).
+	 */
+	public boolean canPermutePalindrome(String s) {
+
+		//to ignore char case and whitespace
+		//s = s.toLowerCase().trim().replaceAll("\\s+", "");
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			Character key = s.charAt(i);
+			map.put(key, map.getOrDefault(key, 0) + 1);
+		}
+
+		//to print map
+//		for(Map.Entry<Character, Integer> entry : map.entrySet())
+//			System.out.println(entry.getKey() + "--" + entry.getValue());
+			
+		int countOdds = 0;
+		for (Character key : map.keySet()) {
+			int val = map.get(key);
+			if (val % 2 != 0) // if it is not even
+				countOdds++;
+		}
+
+		return (countOdds <= 1);
+	}
+
+	
+	public static void main(String[] args) {
+		
+		PalimdromePermutations p = new PalimdromePermutations();
+		
+		System.out.println(p.canPermutePalindrome("code"));
+		System.out.println(p.canPermutePalindrome("aab"));
+		System.out.println(p.canPermutePalindrome("carerac"));
+		System.out.println(p.canPermutePalindrome("Tact Coa")); //we are considering Char Case and whitespaces too
+		
+	}
 }
--- Java-DS/src/com/strings/OneAway.java
+++ Java-DS/src/com/strings/OneAway.java
@@ -15,38 +15,39 @@ package com.strings;
 public class OneAway {
 
 	public static void main(String[] args) {
-		
+
 		OneAway o = new OneAway();
-		
+
 		System.out.println(o.oneEditAway("pale", "ple"));
 		System.out.println(o.oneEditAway("pales", "pale"));
 		System.out.println(o.oneEditAway("pale", "bale"));
 		System.out.println(o.oneEditAway("pale", "bae"));
-		
+
 	}
-	
-	public boolean oneEditAway(String s1, String s2){
-		if(s1.length() == s2.length())
-			return oneEditReplace(s1,s2);
-		else if(s1.length() > s2.length())
+
+	public boolean oneEditAway(String s1, String s2) {
+		if (s1.length() == s2.length())
+			return oneEditReplace(s1, s2);
+		else if (s1.length() > s2.length())
 			return oneEditInsert(s1, s2);
-		else if(s2.length() > s2.length())
-			return oneEditInsert(s2,s1);
-		
+		else if (s2.length() > s1.length())
+			return oneEditInsert(s2, s1);
+
 		return false;
 	}
 
 	private boolean oneEditInsert(String s1, String s2) {
-		
-		int i =0; int j =0;
-		while(i<s1.length() && j<s2.length()){
-			
-			if(s1.charAt(i) == s2.charAt(j)){
-				i++;j++;
-			}
-			else{
+
+		int i = 0;
+		int j = 0;
+		while (i < s1.length() && j < s2.length()) {
+
+			if (s1.charAt(i) == s2.charAt(j)) {
 				i++;
-				if(s1.charAt(i) != s2.charAt(j)){
+				j++;
+			} else {
+				i++;
+				if (s1.charAt(i) != s2.charAt(j)) {
 					return false;
 				}
 			}
@@ -57,11 +58,10 @@ public class OneAway {
 	private boolean oneEditReplace(String s1, String s2) {
 		int diffCount = 0;
 		for (int i = 0; i < s1.length(); i++) {
-			if(s1.charAt(i) != s2.charAt(i))
+			if (s1.charAt(i) != s2.charAt(i))
 				diffCount++;
 		}
 		return (diffCount == 1);
 	}
-	
-	
+
 }
