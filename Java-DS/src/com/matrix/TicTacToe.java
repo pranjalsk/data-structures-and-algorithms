ebe44b4894ddb26b5f983be21f0652d209ea520a
pranjalsk <pkaranka@asu.edu>
2018-03-10 12:45:42 -0700

    local to git

--- Java-DS/src/com/matrix/TicTacToe.java
+++ Java-DS/src/com/matrix/TicTacToe.java
@@ -3,8 +3,68 @@ package com.matrix;
 public class TicTacToe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
 
+	}
+
+	int[][] game;
+
+	public TicTacToe(int n) {
+		game = new int[n][n];
+	}
+
+	public int move(int row, int col, int player) {
+
+		game[row][col] = player;
+		int n = game.length;
+
+		// check col
+		boolean win = true;
+		for (int i = 0; i < n; i++) {
+			if (game[row][i] != player) {
+				win = false;
+				break;
+			}
+		}
+		if (win)
+			return player;
+
+		// check row
+		win = true;
+		for (int i = 0; i < n; i++) {
+			if (game[i][col] != player) {
+				win = false;
+				break;
+			}
+		}
+		if (win)
+			return player;
+
+		// check diagonal 1
+		win = true;
+		for (int i = 0; i < n; i++) {
+			if (game[i][i] != player) {
+				win = false;
+				break;
+			}
+		}
+		if (win)
+			return player;
+
+		// check diagonal 2
+		win = true;
+		for (int i = 0; i < n; i++) {
+			if (game[i][n - 1 - i] != player) {
+				win = false;
+				break;
+			}
+		}
+		if (win)
+			return player;
+
+		return 0;
 	}
 
 }
