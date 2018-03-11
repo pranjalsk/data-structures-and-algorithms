7653f92ab4dc2e053040ed9d423ef76b33f977f8
pranjalsk <pkaranka@asu.edu>
2018-03-11 13:44:42 -0700

    local to git

--- Java-DS/src/com/binarySearch/SearchRotatedSorted.java
+++ Java-DS/src/com/binarySearch/SearchRotatedSorted.java
@@ -2,34 +2,28 @@ package com.binarySearch;
 
 public class SearchRotatedSorted {
 
-	public int search(int[] A, int target) {
-	    int left = 0;
-	    int right = A.length - 1;
-	    
-	    while (left <= right) {
+	public int search(int[] nums, int target) {
+		 int start = 0;
+	        int end = nums.length - 1;
+	        while (start <= end){
+	            int mid = (start + end) / 2;
+	            if (nums[mid] == target)
+	                return mid;
 	        
-	    	int mid = left + (right - left) / 2;
+	            if (nums[start] <= nums[mid]){
+	                 if (target < nums[mid] && target >= nums[start]) 
+	                    end = mid - 1;
+	                 else
+	                    start = mid + 1;
+	            } 
 	        
-	    	if (A[mid] == target)
-	            return mid;
-	         
-	    	
-	    	else if (A[mid] > target) {
-	            // the left half is monotonic, yet still does not satisfy
-	            if (A[left] <= A[mid] && A[left] > target) { 
-	                left = mid + 1;
-	            } else {
-	                right = mid - 1;
-	            }
-	        } else {
-	            // the right half is monotonic, yet still does not satisfy
-	            if (A[right] >= A[mid] && A[right] < target) { 
-	                right = mid - 1;
-	            } else {
-	                left = mid + 1;
+	            if (nums[mid] <= nums[end]){
+	                if (target > nums[mid] && target <= nums[end])
+	                    start = mid + 1;
+	                 else
+	                    end = mid - 1;
 	            }
 	        }
-	    }
-	    return -1;
+	        return -1;
 	}
 }
